ed67c0ac2d7a818238f236204620e44262fcc0c5
Malehe3 <160879306+Malehe3@users.noreply.github.com>
2024-03-27 14:53:34 -0500

    Update app.py

--- app.py
+++ app.py
@@ -7,7 +7,7 @@ st.title('CocinaFacil - Análisis de Sentimientos')
 
 st.video("gatitos4.mp4")
 
-st.subheader("¡Bienvenido a CocinaFacil con ChefIA, tu asistente de cocina personal! Describe tu día en una frase y te recomendaremos una receta adecuada para tu estado de ánimo")
+st.write("¡Bienvenido a CocinaFacil con ChefIA, tu asistente de cocina personal! Describe tu día en una frase y te recomendaremos una receta adecuada para tu estado de ánimo")
 
 translator = Translator()
 
